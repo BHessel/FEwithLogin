b46629e944acb70f497cce796b136b84e1c31b0a
Ben Hessel <hesselfitness@gmail.com>
2022-05-05 10:41:00 -0400

    still having issues w/ video load, starting new branch

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import VideoCard from '../Presentational/VideoCard'
 
 const VideoContainer = ({ loggedInStatus }) => {
+
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        const videoURL = "http://localhost:3000/videos";
+    
+        const fetchData = () => {
+          try {
+            fetch(videoURL)
+            .then(response => response.json())
+            .then(data => setVideos(data))
+            
+          } catch (error) {
+            console.log("error", error);
+          }
+        };
+    
+        fetchData();
+    }, []);
+        
+    
+    // const handleSearch = () => {
+    //     let searchValue = userSearchRef.current.value
+    //     console.log('this is the searchvalue', searchValue)
+    //     setUserSearch(searchValue)
+    // }
+
     return (
         <div>
-            <h1>VideoContainer</h1>
+            <h1>Video Container</h1>
             <h1>Status: {loggedInStatus} </h1>
+
+            <h1 className='video-header bg-color'>Welcome to Netflix MovieMatcher</h1>
+                <p className='video-subheader bg-color'>Scroll or search for shows and movies to watch each trailer. If you like it, click "Add to Favorites," and then connect with friends to see where your TV interests match! </p>
+
+
+                <div className="video-container">
+                        {videos.map((vid, i) =>
+                                <VideoCard 
+                                    video={vid}
+                                    // key={i}
+                                    // addToFavorites={addToFavorites}
+                                />  
+                        )}
+                </div>
         </div>
+
+        
     );
 }
 
---
+++ src/Presentational/VideoCard.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import VideoPlay from './Video'
+import { Link } from 'react-router-dom'
+
+
+const VideoCard = ({ video, key, addToFavorites }) => {
+        
+
+    
+    return (
+
+    <div className="card-container" key={key}>
+        <div className="vid-card-img">
+            <VideoPlay
+              videoURL={video.url}
+            />
+        </div>
+
+        <div className='card-content'>
+            <div className='vid-card-title-container'>
+                <h2>{video.title}</h2>
+            </div>
+            
+            <div className='vid-card-buttons'>
+                    <button className='vid-card-btn'>
+                        <Link to={{
+                            pathname: '/VidPlayer',
+                            state: {video: video}    
+                        }}>
+                            Play in Full Size        
+                        </Link>
+                    </button>
+               
+                    {window.location.pathname === '/Favorites' ?
+                    <button className='remove-btn ' onClick=''>Remove from Favorites</button> : 
+                    <button className='vid-card-btn' onClick={() => addToFavorites(video)}>Add to Favorites</button>
+                    }  
+                
+            </div>
+        </div>        
+    </div>
+    )
+}
+
+export default VideoCard
