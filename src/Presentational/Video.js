fe0470dd6ac8b356d4cf0466804660fae458e497
Ben Hessel <hesselfitness@gmail.com>
2022-05-07 21:00:41 -0400

    Full display for Video (theatre, whatever) works again. Errors still in console..

--- src/Presentational/Video.js
+++ src/Presentational/Video.js
@@ -9,7 +9,7 @@ const Video = () => {
     const videoKey = location.state.video.thumbnail.split('/')[4]
     // console.log(videoKey)
     const videoUrlArray = [videoInfo.url]
-    console.log(videoUrlArray)
+    // console.log(videoUrlArray)
     
 
     const [play, setPlay] = useState(false);
@@ -23,17 +23,35 @@ const Video = () => {
 
 
     return (
-        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <ReactPlayer
-                width="640px"
-                height='360px'
-                playing={play}
-                // config={{ file: { forceHLS: true } }}
-                controls={true}
-                light={videoInfo.thumbnail}
-                url={videoUrlArray}
-                className='react-player'
-            />
+        <div className='background-gradient'>
+            <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='full-vid-player'>
+                <ReactPlayer
+                    width="640px"
+                    height='360px'
+                    playing={play}
+                    // config={{ file: { forceHLS: true } }}
+                    controls={true}
+                    light={videoInfo.thumbnail}
+                    url={videoUrlArray}
+                    className='react-player'
+                />
+            </div>
+
+            <div className='video-info-card'>
+                <h2 className='info-font'>
+                    {videoInfo.title}
+                </h2>
+                <div className='info-font'>
+                    <div className='flex-info-font'>
+                        <h4>Release Date:</h4>
+                        <p>2/15/2055</p>
+                    </div>
+                    <div className='flex-info-font'>
+                        <h4>Description:</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
